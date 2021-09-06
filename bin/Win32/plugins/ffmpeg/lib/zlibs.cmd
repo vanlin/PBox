f31d9d160bd46c0924d3e7146ae1b320d94328c8
dbyoung720 <dbyoung@sina.com>
2021-09-06 13:35:55 +0800

    update ffmpeg sdk

---
+++ bin/Win32/plugins/ffmpeg/lib/zlibs.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+
+cd ..
+cd bin
+
+lib /def:"..\lib\avcodec-59.def"   /machine:i386 /out:"..\lib\avcodec-59.lib"
+lib /def:"..\lib\avdevice-59.def"  /machine:i386 /out:"..\lib\avdevice-59.lib"
+lib /def:"..\lib\avformat-59.def"  /machine:i386 /out:"..\lib\avformat-59.lib"
+lib /def:"..\lib\avutil-57.def"    /machine:i386 /out:"..\lib\avutil-57.lib"
+lib /def:"..\lib\avfilter-8.def"   /machine:i386 /out:"..\lib\avfilter-8.lib"
+lib /def:"..\lib\postproc-56.def"  /machine:i386 /out:"..\lib\postproc-56.lib"
+lib /def:"..\lib\swresample-4.def" /machine:i386 /out:"..\lib\swresample-4.lib"
+lib /def:"..\lib\swscale-6.def "   /machine:i386 /out:"..\lib\swscale-6.lib"

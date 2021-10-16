85969ce4a0d03a0ebcf6129f46e7475c5bf5aae9
dbyoung720 <dbyoung@sina.com>
2021-10-16 09:43:43 +0800

    add txtOCR mdoule dll

---
+++ module/txtOCR/txtOCR.dpr
@@ -0,0 +1,19 @@
+library txtOCR;
+{$IF CompilerVersion >= 21.0}
+{$WEAKLINKRTTI ON}
+{$RTTI EXPLICIT METHODS([]) PROPERTIES([]) FIELDS([])}
+{$IFEND}
+
+uses
+  System.SysUtils,
+  System.Classes,
+  uMainForm in 'uMainForm.pas' {frmtxtOCR};
+
+{$R *.res}
+
+exports
+  db_ShowDllForm_Plugins;
+
+begin
+
+end.

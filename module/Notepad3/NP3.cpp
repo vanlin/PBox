8a6ed5725fe34fc7473cf460a3bf6448dd410249
dbyoung720 <dbyoung@sina.com>
2022-01-11 15:06:23 +0800

    add vc dll module notepad3

---
+++ module/Notepad3/NP3.cpp
@@ -0,0 +1,37 @@
+﻿#include <windows.h>
+
+HINSTANCE hinst = NULL;
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+      case DLL_PROCESS_ATTACH:
+        hinst = (HINSTANCE)hModule;
+      case DLL_THREAD_ATTACH:
+      case DLL_THREAD_DETACH:
+      case DLL_PROCESS_DETACH:
+          break;
+    }
+    return TRUE;
+}
+
+enum TLangStyle {lsDelphiDll, lsVCDLGDll, lsVCMFCDll, lsQTDll, lsEXE};
+
+//extern int WINAPI wWinMain(HINSTANCE hInstance,HINSTANCE hPrevInst,LPSTR lpCmdLine,int nCmdShow);
+extern int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd);
+
+extern "C" __declspec(dllexport) void db_ShowDllForm_Plugins(TLangStyle* lsFileType, char** strParentName, char** strSubModuleName, char** strClassName, char** strWindowName, const bool show = false)
+{
+    * lsFileType       = lsVCDLGDll;      // TLangStyle
+    * strParentName    = "文本编辑";      // 父模块名称
+    * strSubModuleName = "Notepad3(DLL)"; // 子模块名称
+    * strClassName     = "Notepad3U";     // 窗体类名
+    * strWindowName    = "Notepad3";      // 窗体名
+    
+    if (show) 
+    {
+      wWinMain(hinst, 0, (LPWSTR)"", (int)show);
+    }
+}
+
---
+++ module/Notepad3/x64.cmd
@@ -0,0 +1,52 @@
+@echo off
+Color A
+
+set CurrentCD=%~dp0
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+
+:: 删除临时文件
+del NP3.dll
+del NP3.exp
+del NP3.lib
+del NP3.obj
+del vc140.pdb
+
+:: 更新版本
+CD /D "%CurrentCD%GIT"
+call Version.cmd"
+
+:: 修改源码
+git apply ..\NP3.patch 
+
+:: 编译原有的 NOTEPAD3 源码
+MSBuild %CurrentCD%GIT\Notepad3.sln /nologo /consoleloggerparameters:Verbosity=minimal /maxcpucount /nodeReuse:true^
+  /target:Build /property:Configuration=Release;Platform=x64^
+  /flp1:LogFile=zerror.log;errorsonly;Verbosity=diagnostic^
+  /flp2:LogFile=zwarns.log;warningsonly;Verbosity=diagnostic
+
+:: 编译 NP3.CPP
+CD /D %CurrentCD%
+cl /c  /Zi /nologo /W3 /WX- /diagnostics:classic /O2 /Oy- /GL /D WIN64 /D STATIC_BUILD /D BOOKMARK_EDITION /D NDEBUG /D _CRT_SECURE_NO_WARNINGS /D _UNICODE /D UNICODE /Gm- /EHsc /MT /GS /arch:SSE2 /fp:precise /Zc:wchar_t /Zc:forScope /Zc:inline /Gd /analyze- /FC  %CurrentCD%NP3.cpp
+
+:: 与原来编译 EXE 产生出的 OBJ/LIB/RES 一起，连接为动态库
+link /dll -out:NP3.dll /DELAYLOAD:UXTHEME.dll -nologo -RELEASE -OPT:REF -OPT:ICF -LTCG NP3.obj %CurrentCD%GIT\Bin\Release_x64_v142\obj\Notepad3\*.obj ^
+  %CurrentCD%GIT\Bin\Release_x64_v142\obj\Notepad3\notepad3.res ^
+  %CurrentCD%GIT\Bin\Release_x64_v142\obj\Scintilla.lib ^
+  %CurrentCD%GIT\Bin\Release_x64_v142\obj\LEXILLA.lib ^
+  %CurrentCD%GIT\Bin\Release_x64_v142\obj\USER32-STUB.lib ^
+  %CurrentCD%GIT\Bin\Release_x64_v142\obj\UXTHEME-STUB.lib ^
+  ntdll.lib imm32.lib Shlwapi.lib Comctl32.lib MUILOAD.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib DELAYIMP.Lib
+
+:: 复制到 PBox plugins 目录下
+copy /Y NP3.dll ..\..\bin\WIN64\plugins\NP3.dll
+
+:: 复制语言列表到  PBox plugins 目录下
+xcopy /e /y /c /i "%CurrentCD%GIT\Bin\Release_x64_v142\lng\*.*"   "..\..\bin\WIN64\plugins\lng\" 
+
+:: 删除临时文件
+del NP3.dll
+del NP3.exp
+del NP3.lib
+del NP3.obj
+del vc140.pdb
